a7712d8c582efece926a5687b9c6c7ba02b10dcc
limbajithorat <91537453+limbajithorat@users.noreply.github.com>
2023-01-24 11:41:01 +0530

    initial commit

--- Socket-chat-app/utilities.js
+++
@@ -1,9 +0,0 @@
-const generateMsg = (username, text) => {
-  return {
-    username,
-    text,
-    createdAt: new Date().getTime(),
-  };
-};
-
-module.exports = { generateMsg };
---
+++ client/src/components/Home.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Home = ({ socket }) => {
+  const navigate = useNavigate();
+  const [userName, setUserName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    localStorage.setItem("userName", userName);
+    socket.emit("newUser", { userName, socketID: socket.id });
+    navigate("/chat");
+  };
+  return (
+    <form className="home__container" onSubmit={handleSubmit}>
+      <h1 className="home__header">ChatAPP</h1>
+      <label htmlFor="username">Enter Your Name</label>
+      <input
+        type="text"
+        minLength={3}
+        name="username"
+        id="username"
+        className="username__input"
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}
+      />
+      <button className="home__cta">SIGN IN</button>
+    </form>
+  );
+};
+
+export default Home;
